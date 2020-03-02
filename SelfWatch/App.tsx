d355090a0e4fc4065c774ed64861c38058a2330d
Sergey <serguljan@gmail.com>
2020-03-02 00:18:29 -0800

    deleted selfwatch

--- SelfWatch/App.tsx
+++
@@ -1,8 +0,0 @@
-import React, {ReactNode} from 'react';
-import {ScreenNavigator} from './src/ui/components/ScreenNavigator';
-
-const App: () => ReactNode = () => {
-  return <ScreenNavigator />;
-};
-
-export default App;
